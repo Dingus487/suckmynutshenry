a508e17f8954c113d0063ce4e3ba38de4dc5a202
Pedropeterj <95436907+Pedropeterj@users.noreply.github.com>
2022-02-17 12:39:07 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,73 +1,95 @@
 controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    if (bussyfridge.vx > 0) {
+        projectile2 = sprites.createProjectileFromSprite(img`
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            `, bussyfridge, 150, 0)
+        pause(100)
+        projectile2.destroy()
+    } else if (bussyfridge.vx == 0) {
+        projectile2 = sprites.createProjectileFromSprite(img`
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            `, bussyfridge, 0, -150)
+        pause(100)
+        projectile2.destroy()
+    } else {
+        projectile2 = sprites.createProjectileFromSprite(img`
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+            `, bussyfridge, -150, 0)
+        pause(100)
+        projectile2.destroy()
+    }
+})
+controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
+    if (bussyfridge.vy > 0) {
+        bussyfridge.vy = -115
+    }
+})
+controller.player2.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     bussyfridge,
-    [img`
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 f 
-        8 f 8 8 8 8 8 8 8 8 8 8 8 8 f 8 
-        8 8 f 8 8 8 8 8 8 8 8 8 8 f 8 8 
-        8 8 8 f 8 8 8 8 8 8 8 8 f 8 8 8 
-        8 8 8 8 f 8 8 8 8 8 8 f 8 8 8 8 
-        8 8 8 8 8 f 8 8 8 8 f 8 8 8 8 8 
-        8 8 8 8 8 8 f 8 8 f 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 f f 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 f f 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 f 8 8 f 8 8 8 8 8 8 
-        8 8 8 8 8 f 8 8 8 8 f 8 8 8 8 8 
-        8 8 8 8 f 8 8 8 8 8 8 f 8 8 8 8 
-        8 8 8 f 8 8 8 8 8 8 8 8 f 8 8 8 
-        8 8 f 8 8 8 8 8 8 8 8 8 8 f 8 8 
-        8 f 8 8 8 8 8 8 8 8 8 8 8 8 f 8 
-        f 8 8 8 8 8 8 8 8 8 8 8 8 8 8 f 
-        `,img`
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-        `],
-    100,
+    assets.animation`fridge boy run`,
+    200,
     false
     )
 })
-controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
-    if (bussyfridge.vy <= 0) {
-        bussyfridge.setVelocity(0, -50)
-        pause(500)
-        bussyfridge.vy += 100
-        pause(500)
-        bussyfridge.vy += -50
-    }
+controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    bussyfridge,
+    assets.animation`fridge boy run`,
+    200,
+    false
+    )
 })
+let projectile2: Sprite = null
 let bussyfridge: Sprite = null
-bussyfridge = sprites.create(img`
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level1`)
+bussyfridge = sprites.create(assets.image`fridge boy`, SpriteKind.Player)
 bussyfridge.setStayInScreen(true)
 controller.player2.moveSprite(bussyfridge, 100, 0)
-bussyfridge.setPosition(0, 112)
+bussyfridge.setPosition(32, 65)
+bussyfridge.ay = 200
